b411af7ff5b7a73bc27af2cb176dcbb71e477eba
taewaun <taewaunkwon@gmail.com>
2023-03-19 23:07:00 +0900

    feat : MainActivity 기본 세팅

--- presentation/src/main/java/com/twaun95/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/twaun95/presentation/ui/MainActivity.kt
@@ -1,14 +1,26 @@
 package com.twaun95.presentation.ui
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.twaun95.presentation.R
+import com.twaun95.presentation.base.BaseActivity
+import com.twaun95.presentation.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(){
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main){
+    private val viewModel by viewModels<MainActivityViewModel>()
+
+    override fun initView() {
+        super.initView()
+    }
+
+    override fun setEvent() {
+        super.setEvent()
+    }
+
+    override fun setObserver() {
+        super.setObserver()
     }
 }
